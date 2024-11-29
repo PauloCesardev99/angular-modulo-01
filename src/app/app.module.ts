30dc8babf81281ae747e986419de4eb167d586db
paulocesardev25 <paulocesarbritocamilo@gmail.com>
2024-11-29 17:11:01 -0300

    primeiro commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+
+@NgModule({
+  //Aqui é onde os componentes estão declarados
+  declarations: [
+    AppComponent,
+    CardComponent,
+    CardRoxoComponent,
+  ],
+  //aqui estão os modulos importados
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideClientHydration(withEventReplay())
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/card/card.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  //nome do componente no html
+  selector: 'app-card',
+  standalone: false,
+  //Template html quando ele carregar
+  templateUrl: './card.component.html',
+  //template do styleUrls
+  styleUrl: './card.component.css'
+})
+export class CardComponent {
+
+}
